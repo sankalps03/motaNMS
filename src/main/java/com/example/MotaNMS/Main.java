b3c6fb018b80fa47d9928fc3469a5a9b39625523
sankalps03 <sankalpsingh5253@gmail.com>
2023-06-16 00:23:10 +0530

    15/06/23 Refactored Main

--- src/main/java/com/example/MotaNMS/Main.java
+++ src/main/java/com/example/MotaNMS/Main.java
@@ -4,76 +4,63 @@ import com.example.MotaNMS.database.DatabaseVerticle;
 import com.example.MotaNMS.poller.PollingVerticle;
 import com.example.MotaNMS.publicAPI.PublicApiVerticle;
 import com.example.MotaNMS.services.ServiceVerticle;
+import io.vertx.core.CompositeFuture;
 import io.vertx.core.Vertx;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Main {
 
-  private static final Logger logger = LoggerFactory.getLogger(Main.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
-  public static void main(String[] args) {
+  private static Vertx vertx;
 
-    try {
+  public static void main(String[] args)
+  {
+    try
+    {
+       vertx = Vertx.vertx();
 
-      Vertx vertx = Vertx.vertx();
+      CompositeFuture.all(
+        vertx.deployVerticle(DatabaseVerticle.class.getName()),
+        vertx.deployVerticle(PublicApiVerticle.class.getName()),
+        vertx.deployVerticle(ServiceVerticle.class.getName()),
+        vertx.deployVerticle(PollingVerticle.class.getName())
+      )
+        .onComplete(result ->
+      {
+        if (result.succeeded())
+        {
+          LOGGER.info("All veritcles deployed Successfully");
+        }
+        else
+        {
+          stopVertx();
 
-      vertx.deployVerticle(DatabaseVerticle.class.getName()).onComplete(handler ->{
-
-        if (handler.succeeded()){
-
-          logger.info("Database verticle deployed successfully");
-
-        }else {
-
-          logger.error(handler.result().toString());
-
+          LOGGER.error(result.cause().getMessage(),result.cause());
         }
       });
 
-      vertx.deployVerticle(PublicApiVerticle.class.getName()).onComplete(handler ->{
-
-        if (handler.succeeded()){
-
-          logger.info("publicAPI verticle deployed successfully");
-
-        }else {
-
-          logger.error(handler.result().toString());
-
-        }
-      });
-
-      vertx.deployVerticle(ServiceVerticle.class.getName()).onComplete(handler ->{
-
-        if (handler.succeeded()){
-
-          logger.info("Service verticle deployed successfully");
-
-        }else {
-
-          logger.error(handler.result().toString());
-
-        }
-      });
-
-      vertx.deployVerticle(PollingVerticle.class.getName()).onComplete(handler ->{
-
-        if (handler.succeeded()){
-
-          logger.info("Polling verticle deployed successfully");
-
-        }else {
-
-          logger.error(handler.result().toString());
-
-        }
-      });
-
-    } catch (Exception exception) {
-
-      logger.error(exception.getMessage());
+    }
+    catch (Exception exception)
+    {
+      LOGGER.error(exception.getMessage(),exception.getCause());
     }
 
+  }
+
+  public static void stopVertx() {
+
+    vertx.close(result ->
+    {
+      if (result.succeeded())
+      {
+        LOGGER.info("Vert.x instance gracefully stopped.");
+      }
+      else
+      {
+        LOGGER.error(result.cause().getMessage(),result.cause());
+      }
+    });
   }
 }
